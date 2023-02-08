d67ab90c7be100d53051cf7eebf2d9e3ef3acbff
Irina Matusevich <irina@e-c.agency>
2023-02-08 12:12:49 +0100

    init

---
+++ web/sites/default/settings.lando.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal9',
+  'username' => 'drupal9',
+  'password' => 'drupal9',
+  'host' => 'database',
+  'driver' => 'mysql',
+);
+
+/**
+ * Set config directory.
+ */
+$settings['config_sync_directory'] = '../config/default/sync';
